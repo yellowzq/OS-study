1c7d0fb4f7d32a9fb8760d4f426708ec69eb991a
rgzz <rgzzsakura@qq.com>
2024-11-03 20:44:14 +0800

    add:添加整数打印put_int

--- bochs/boot/kernel/main.c
+++ bochs/boot/kernel/main.c
@@ -1,6 +1,14 @@
 #include "print.h"
-int main(void) {
+void main(void) {
     put_str("I am kernel\n");
+    put_int(0);
+    put_char('\n');
+    put_int(9);
+    put_char('\n');
+    put_int(0x00021a3f);
+    put_char('\n');
+    put_int(0x12345678);
+    put_char('\n');
+    put_int(0x10000000);
     while(1);
-    return 0;
 }
