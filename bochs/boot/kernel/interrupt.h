66097459e8dfc8f07272b3972318bcafe7eb9aca
rgzz <rgzzsakura@qq.com>
2024-12-10 22:26:51 +0800

    add:新增定时器，控制中断频率

--- bochs/boot/kernel/interrupt.h
+++
@@ -1,5 +0,0 @@
-#ifndef __KERNEL_INTERRUPT_H
-#define __KERNEL_INTERRUPT_H
-typedef void* intr_handler;
-void idt_init();
-#endif
---
+++ bochs/c/boot/makeup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# 制作镜像
+rm ../../sakura.img &&
+../../bin/bximage -hd -mode="flat" -size=60 -q ../../sakura.img
+
+# 编译并写入mbr&loader
+nasm -I include/ -o mbr.bin mbr.S
+dd if=./mbr.bin of=../../sakura.img bs=512 count=1 conv=notrunc
+
+# 编译并写入loader
+nasm -I include -o loader.bin loader.S
+dd if=./loader.bin of=../../sakura.img bs=512 count=4 seek=2 conv=notrunc
+
+# 编译并写入kernel
+gcc -c -o ../kernel/main.o ../kernel/main.c &&
+ld ../kernel/main.o -Ttext 0xc0001500 -e main -o ../kernel/kernel.bin &&
+dd if=../kernel/kernel.bin of=../../sakura.img bs=512 count=200 seek=9 conv=notrunc
