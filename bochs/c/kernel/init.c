9ac82310f224e995c1252ea73b701dd77655932f
sakura <rgzzsakura@qq.com>
2025-07-15 22:06:46 +0800

    add:定义并初始化tss

--- bochs/c/kernel/init.c
+++ bochs/c/kernel/init.c
@@ -6,6 +6,7 @@
 #include "thread.h"
 #include "console.h"
 #include "keyboard.h"
+#include "tss.h"
 
 /*负责初始化所有模块 */
 void init_all(void) {
@@ -16,4 +17,5 @@ void init_all(void) {
     timer_init();   //初始化PIT
     console_init(); //初始化控制台
     keyboard_init(); //初始化键盘
+    tss_init();      //初始化任务状态段
 }
