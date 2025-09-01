fd4f59098d45f7062fc4ef56d3f602fb561cb51e
sakura <rgzzsakura@qq.com>
2025-09-01 21:57:46 +0800

    add: 实现sys_malloc功能

--- bochs/c/kernel/main.c
+++ bochs/c/kernel/main.c
@@ -7,6 +7,7 @@
 #include "syscall-init.h"
 #include "syscall.h"
 #include "stdio.h"
+#include "memory.h"
 
 void k_thread_a(void*);
 void k_thread_b(void*);
@@ -17,12 +18,6 @@ int main(void) {
     put_str("I am kernel\n");
     init_all();
 
-    process_execute(u_prog_a, "u_prog_a");
-    process_execute(u_prog_b, "u_prog_b");
-
-    console_put_str(" main_pid:0x");
-    console_put_int(sys_getpid());
-    console_put_char('\n');
     intr_enable();
     thread_start("k_thread_a", 31, k_thread_a, " A_");
     thread_start("k_thread_b", 31, k_thread_b, " B_");
@@ -33,8 +28,9 @@ int main(void) {
 /* 在线程中运行的函数*/
 void k_thread_a(void* arg){
     char* para = arg;
-    console_put_str(" thread_a_pid:0x");
-    console_put_int(sys_getpid());
+    void* addr = sys_malloc(33);
+    console_put_str(" thread_a_pid, sys_malloc(33), addr:0x");
+    console_put_int((int)addr);
     console_put_char('\n');
     while (1);
 }
@@ -42,22 +38,9 @@ void k_thread_a(void* arg){
 /* 在线程中运行的函数*/
 void k_thread_b(void* arg){
     char* para = arg;
-    console_put_str(" thread_b_pid:0x");
-    console_put_int(sys_getpid());
+    void* addr = sys_malloc(63);
+    console_put_str(" thread_b_pid, sys_malloc(63), addr:0x");
+    console_put_int((int)addr);
     console_put_char('\n');
-    while (1);
-}
-
-/* 用户程序 A */
-void u_prog_a(void) {
-    char* name = "prog_a";
-    printf(" prog_a_name: %s, prog_a_pid: 0x%x\n", name, getpid());
-    while (1);
-}
-
-/* 用户程序 B */
-void u_prog_b(void) {
-    char* name = "prog_b";
-    printf(" prog_b_name: %s, prog_b_pid: 0x%x\n", name, getpid());
     while (1);
 }
--- bochs/c/kernel/memory.h
+++ bochs/c/kernel/memory.h
@@ -2,6 +2,7 @@
 #define __KERNEL_MEMORY_H
 #include "stdint.h"
 #include "bitmap.h"
+#include "list.h"
 
 /*  内存池标记，用于判断用哪个内存池 */
 enum pool_flags{
@@ -23,13 +24,29 @@ struct virtual_addr
     uint32_t vaddr_start;           //虚拟地址起始地址
 };
 
+/* 内存块 */
+struct mem_block {
+    struct list_elem free_elem;     // 用于加入内存块链表的结点
+};
+
+/* 内存块描述符 */
+struct mem_block_desc {
+    uint32_t block_size;            // 内存块大小
+    uint32_t blocks_per_arena;      // 本arena中可容纳此mem
+    struct list free_list;          // 本des的空闲mem块链表
+};
+#define DESC_CNT 7                   // 内存块描述符个数,16,32,64,128,256,512,1024
+
 extern struct pool kernel_pool, user_pool;
 void mem_init(void);
 void* malloc_page(enum pool_flags pf, uint32_t pg_cnt);
+void malloc_init(void);
 void* get_kernel_pages(uint32_t pg_cnt);
 uint32_t* pte_ptr(uint32_t vaddr);
 uint32_t* pde_ptr(uint32_t vaddr);
 uint32_t addr_v2p(uint32_t vaddr);
 void* get_a_page(enum pool_flags pf, uint32_t vaddr);
 void* get_user_pages(uint32_t pg_cnt);
+void block_desc_init(struct mem_block_desc* desc_array);
+void* sys_malloc(uint32_t size);
 #endif
